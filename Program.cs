a719e18016080c648977a26ad96da046f2f81469
Guilhermebtv <115501380+Guilhermebtv@users.noreply.github.com>
2024-09-02 09:54:15 -0300

    1

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.SetCursorPosition(45, 10);
+            Thread.Sleep(1000);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.Write(" MEU ");
+            Console.ResetColor();
+
+            Thread.Sleep(1000);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.Write(" PRIMEIRO ");
+            Console.ResetColor();
+
+            Thread.Sleep(1000);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine(" PROJETO ");
+
+            Console.ReadKey();
+        }
+    }
+}
